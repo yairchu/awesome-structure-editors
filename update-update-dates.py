8a91227cdb78707120c5e86bcfb3034bc4addf7e
Yair Chuchem <yairchu@gmail.com>
2022-05-10 14:37:48 +0300

    script: style changes

--- update-update-dates.py
+++ update-update-dates.py
@@ -1,40 +1,40 @@
 import requests
 import sys
 
-result = []
 
-lines = iter(open("README.md"))
-
-for line in lines:
-    result.append(line)
-    if line.endswith(" | Last known update\n"):
-        break
-else:
-    print("Did not find table")
-    sys.exit(1)
-result.append(next(lines))
-
-for line in lines:
-    if not line.strip():
-        result.append(line)
-        break
+def new_line(line):
     namelink = line.split("|")[1].strip()
     if not namelink.endswith(")"):
-        result.append(line)
-        continue
+        return
     link = namelink.split("](", 1)[1][:-1]
     prefix = "https://github.com/"
     if not link.startswith(prefix):
-        result.append(line)
-        continue
+        return
     repo = link[len(prefix):]
     date = requests.get(f"https://api.github.com/repos/{repo}/commits?per_page=1").json()[
         0]["commit"]["committer"]["date"]
     year = date.split("-", 1)[0]
     head = line.rsplit("| ", 1)[0]
-    result.append(f"{head}| {year}\n")
+    return f"{head}| {year}\n"
 
-for line in lines:
-    result.append(line)
 
-open("README.md", "w").write("".join(result))
+def new_lines():
+    lines = iter(open("README.md"))
+    for line in lines:
+        yield line
+        if line.endswith(" | Last known update\n"):
+            break
+    else:
+        print("Did not find table")
+        sys.exit(1)
+    yield next(lines)
+    for line in lines:
+        if not line.strip():
+            yield line
+            break
+        yield new_line(line) or line
+    yield from lines
+
+
+res = "".join(new_lines())
+open("README.md", "w").write(res)
